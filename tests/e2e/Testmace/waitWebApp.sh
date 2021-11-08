16c5cff86f86c20ec21137118cb26e92b6221fb3
0B1t322 <dan.demin@gmail.com>
2021-11-08 16:37:41 +0300

    fix error on tests

--- tests/e2e/Testmace/waitWebApp.sh
+++ tests/e2e/Testmace/waitWebApp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-HTTP_STATUS="$(curl -IL --silent http://localhost:8080 | grep HTTP | grep -P '\d\d\d' -o )"; 
+HTTP_STATUS="$(curl -IL --silent http://localhost:8080 | grep HTTP | grep -p '\d\d\d' -o )"; 
 echo "$HTTP_STATUS"
 while [ "$HTTP_STATUS" != "404" ]; do
     echo "$HTTP_STATUS"
     echo "service can't return 404, waiting..."
     sleep 1
-    HTTP_STATUS="$(curl -IL --silent http://localhost:8080 | grep HTTP | grep -P '\d\d\d' -o )"; 
+    HTTP_STATUS="$(curl -IL --silent http://localhost:8080 | grep HTTP | grep -p '\d\d\d' -o )"; 
 done
 echo "DONE $HTTP_STATUS"
